43a3e6469dcb33a6eb694ba9775aefc906268368
Tony Xia <txia@lipalater.com>
2023-02-27 15:36:55 +0800

    update

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="main-footer sticky footer-type-1">
+    <div class="footer-inner">
+      <!-- 版权信息 -->
+      <div class="footer-text">
+        &copy; 2022-2023
+        <a href="#"><strong>Navigation</strong></a>
+        design by <a href="https://summer-88.github.io/" target="_blank"><strong>Tony</strong></a>
+      </div>
+      
+      <div class="go-up">
+        <a href="#" rel="go-top"><i class="fa-angle-up"></i></a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/about.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="boxed-container">
+    <nav class="navbar horizontal-menu navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="navbar-brand">
+          <router-link to="/" class="logo">
+            <img src="https://lipalater.com/wp-content/uploads/2019/09/cropped-Lipa-Later-Logo-1.png" width="300" alt="" class="hidden-xs">
+            <img src="https://lipalater.com/wp-content/uploads/2019/09/cropped-Lipa-Later-Logo-1.png" width="300" alt="" class="visible-xs">
+          </router-link>
+        </div>
+        <div class="navbar-mobile-clear"></div>
+        <a href="https://github.com/Anjaxs/WebStack-vue" target="_blank">
+           <img style="position: absolute; top: 0; right: 0; border: 0;"
+               src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+               alt="Fork me on GitHub">
+        </a>
+      </div>
+    </nav>
+
+    <div class="page-container">
+      <div class="main-content" style="">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="panel panel-default">
+              <!-- 关于网站 -->
+              <h4 class="text-gray">About</h4>
+              <div class="panel-body">
+                <div class="row">
+                  <div class="col-sm-12">
+                    <blockquote>
+                      <p>Record a wide range of web sites.</p>
+                    </blockquote>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Footer from '../components/Footer.vue'
+
+export default {
+  name: 'About',
+  components: {
+    Footer
+  },
+  data() {
+    return {
+      authorImg: ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
